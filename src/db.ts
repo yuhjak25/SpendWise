6c8b87b96404e8ffc5079df7a3b4e53a3d475e5b
yuhjak25 <yuhjakcontact@gmail.com>
2025-02-18 23:37:53 +0100

    refac: changed db validation for working

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,19 @@
 import mongoose from 'mongoose'
 import { MONGODB_URI } from './utils/constants'
 
+if (!MONGODB_URI) {
+  console.log('missing uri')
+  process.exit(1)
+}
+
 export const connectDb = () => {
   mongoose
-    .connect(MONGODB_URI)
+    .connect(MONGODB_URI!)
     .then(() => {
       console.log('Connected to db.')
     })
     .catch((error) => {
       console.log('An error happened: ', error)
+      process.exit(1)
     })
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const SECRET = Bun.env.SECRET!
 export const PORT = Bun.env.PORT!
-export const MONGODB_URI = Bun.env.MONGODB_URI!
+export const MONGODB_URI = Bun.env.MONGODB_URI
