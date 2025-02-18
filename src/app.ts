2757eb00fa9488ff1a54198c7197d2e81427e445
yuhjak25 <yuhjakcontact@gmail.com>
2025-02-18 23:34:57 +0100

    refact: changed routes to work and script dev

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,4 @@ import express from 'express'
 import { authRoute } from './routes/auth.rou'
 export const app = express()
 
-app.use(express.json()).routes('api/auth', authRoute)
+app.use(express.json()).use('/api/auth', authRoute)
