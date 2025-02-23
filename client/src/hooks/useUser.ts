c4d807993a6e64be6152160ed84d01afb6af6546
yuhjak25 <yuhjakcontact@gmail.com>
2025-02-23 17:26:44 +0100

    feat: added handleError

--- client/src/hooks/useUser.ts
+++ client/src/hooks/useUser.ts
@@ -2,27 +2,26 @@ import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../store/hooks/useStore'
 import { userApi } from '../services/user'
 import { setUsername } from '../libs/slices/user'
+import useError from './useError'
 
 export const useUser = () => {
     const dispatch = useAppDispatch()
+    const { handleError } = useError()
     const username = useAppSelector((state) => state.user.username)
 
     useEffect(() => {
         const getUsername = async () => {
             try {
                 const data = await userApi()
-                if (data.error) {
-                    console.error('Failed to fetch username:', data.error)
-                } else {
-                    dispatch(setUsername(data.username))
-                }
+                dispatch(setUsername(data.username))
             } catch (error) {
-                console.error('Failed to fetch username:', error)
+                console.log('Failed to fetch username:', error)
+                handleError('Failed to get the username.')
             }
         }
 
         getUsername()
-    }, [dispatch])
+    }, [dispatch, handleError])
 
     return username
 }
