49f8e7a15b9468c6cc8f5905e082dfd8d5dbdb1b
yuhjak25 <yuhjakcontact@gmail.com>
2025-02-23 16:03:56 +0100

    feat: added auth slice to store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import errorSlice from '../libs/slices/error'
+import authSlice from '../libs/slices/auth'
+
 
 export const store = configureStore({
   reducer: {
-    error: errorSlice
+    error: errorSlice,
+    auth: authSlice
   },
 })
 
